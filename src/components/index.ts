e63c818b3a11df8d80af317141dd25d3232b1a0f
Daniel Nixon <dan.nixon@gmail.com>
2019-07-28 13:53:12 +1000

    Improve formCodec

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,14 +4,39 @@ export * from "./Form";
 export * from "./Input";
 export * from "./Select";
 
-export const formCodec = <R extends t.Props, O extends t.Props>(fields: {
+export function formCodec<R extends t.Props, O extends t.Props>(fields: {
+  readonly required: R;
+  readonly optional: O;
+  // tslint:disable-next-line: readonly-array
+}): t.ReadonlyC<t.IntersectionC<[t.TypeC<R>, t.PartialC<O>]>>;
+export function formCodec<R extends t.Props>(fields: {
+  readonly required: R;
+}): t.ReadonlyC<t.TypeC<R>>;
+export function formCodec<O extends t.Props>(optional: {
+  readonly optional: O;
+}): t.ReadonlyC<t.PartialC<O>>;
+// tslint:disable-next-line: typedef
+export function formCodec<R extends t.Props, O extends t.Props>(fields: {
   readonly required?: R;
   readonly optional?: O;
-}) => {
-  return t.readonly(
-    t.intersection([
-      t.type(fields.required || ({} as R)),
-      t.partial(fields.optional || ({} as O)),
-    ]),
-  );
-};
+}) {
+  const { required, optional } = fields;
+
+  // tslint:disable: no-if-statement
+  if (required !== undefined) {
+    if (optional !== undefined) {
+      return t.readonly(
+        t.intersection([t.type(required), t.partial(optional)]),
+      );
+    } else {
+      return t.readonly(t.type(required));
+    }
+  } else {
+    if (optional !== undefined) {
+      return t.readonly(t.partial(optional));
+    } else {
+      return t.undefined;
+    }
+  }
+  // tslint:enable: no-if-statement
+}
