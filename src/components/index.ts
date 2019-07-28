6d66c5d31b45612b7ded32dd24b764ef38c9d30a
Daniel Nixon <dan.nixon@gmail.com>
2019-07-28 17:53:31 +1000

    Improve formCodec

--- src/components/index.ts
+++ src/components/index.ts
@@ -4,39 +4,41 @@ export * from "./Form";
 export * from "./Input";
 export * from "./Select";
 
+// tslint:disable: readonly-array
+
 export function formCodec<R extends t.Props, O extends t.Props>(fields: {
   readonly required: R;
   readonly optional: O;
-  // tslint:disable-next-line: readonly-array
 }): t.ReadonlyC<t.IntersectionC<[t.TypeC<R>, t.PartialC<O>]>>;
 export function formCodec<R extends t.Props>(fields: {
   readonly required: R;
 }): t.ReadonlyC<t.TypeC<R>>;
-export function formCodec<O extends t.Props>(optional: {
+export function formCodec<O extends t.Props>(fields: {
   readonly optional: O;
 }): t.ReadonlyC<t.PartialC<O>>;
-// tslint:disable-next-line: typedef
-export function formCodec<R extends t.Props, O extends t.Props>(fields: {
-  readonly required?: R;
-  readonly optional?: O;
-}) {
+export function formCodec<R extends t.Props, O extends t.Props>(
+  fields:
+    | {
+        readonly required: R;
+        readonly optional: O;
+      }
+    | {
+        readonly required: R;
+        readonly optional?: undefined;
+      }
+    | {
+        readonly required?: undefined;
+        readonly optional: O;
+      },
+):
+  | t.ReadonlyC<t.IntersectionC<[t.TypeC<R>, t.PartialC<O>]>>
+  | t.ReadonlyC<t.TypeC<R>>
+  | t.ReadonlyC<t.PartialC<O>> {
   const { required, optional } = fields;
 
-  // tslint:disable: no-if-statement
-  if (required !== undefined) {
-    if (optional !== undefined) {
-      return t.readonly(
-        t.intersection([t.type(required), t.partial(optional)]),
-      );
-    } else {
-      return t.readonly(t.type(required));
-    }
-  } else {
-    if (optional !== undefined) {
-      return t.readonly(t.partial(optional));
-    } else {
-      return t.undefined;
-    }
-  }
-  // tslint:enable: no-if-statement
+  return required !== undefined && optional !== undefined
+    ? t.readonly(t.intersection([t.type(required), t.partial(optional)]))
+    : required !== undefined
+    ? t.readonly(t.type(required))
+    : t.readonly(t.partial(optional as O));
 }
